e8b87667cf798460b917d4375911147414a384d2
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-05-21 22:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout (text) {
   return text.toUpperCase();
 }
+
+function whisper (lowercase) {
+  return lowercase.toLowerCase();
+}
+
+function logShout (text) {
+  console.log(text.toUpperCase());
+}
+
+function logWhisper (text) {
+  console.log(text.toLowerCase());
+}
